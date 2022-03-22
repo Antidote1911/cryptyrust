680e91bb4d15597a403303b4171e2aac1733f416
Antidote1911 <antidote1911@gmail.com>
2022-03-23 00:23:04 +0100

    typo

---
+++ build-qtqui-Desktop-Debug/CMakeFiles/cryptyrust_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cryptyrust_autogen"
+  "cryptyrust_autogen/include/ui_mainwindow.h"
+  "cryptyrust_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cryptyrust_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
