fda0fe3a181c22baaaa989b1d521d3237c5c0dc7
Antidote1911 <antidote1911@gmail.com>
2026-05-23 18:00:08 +0200

    up to v1.2.1

--- gui/build.rs
+++ gui/build.rs
@@ -1,8 +1,10 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
+        let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let icon = std::path::Path::new(&manifest).join("assets/icon.ico");
         winres::WindowsResource::new()
-            .set_icon("assets/icon.ico")
+            .set_icon(icon.to_str().unwrap())
             .compile()
             .expect("Failed to compile Windows resources");
     }
